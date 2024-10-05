11b223f9a39f44b34890d008dea639339ee643c3
Light Yagami <itzyourboyrahul@gmail.com>
2024-10-05 12:08:53 +0000

    test commit

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { SocialIcon } from 'react-social-icons'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <a href="rahulyadav.click" className="gradient">rahulyadav.click</a>
+      <div className="socials">
+      <SocialIcon url="https://instagram.com/__rxhxl._" bgColor="rgb(18, 28, 26)" fgColor="white"/>
+      <SocialIcon url="https://www.linkedin.com/in/rahul-yadav-2a4b3b199/" bgColor="rgb(18, 28, 26)" fgColor="white"/>
+      <SocialIcon url="www.x.com" bgColor="rgb(18, 28, 26)" fgColor="whiteg" />
+      </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;

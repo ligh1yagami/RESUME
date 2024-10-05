d1acb7b162f706dc5f2664c93a1d829109adcf10
Light Yagami <itzyourboyrahul@gmail.com>
2024-10-05 19:35:12 +0000

    add animation

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,13 +5,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <a href="rahulyadav.click" className="gradient">rahulyadav.click</a>
+      <a href="#" className="gradient">rahulyadav.click</a>
       <div className="socials">
       <SocialIcon url="https://instagram.com/__rxhxl._" bgColor="rgb(18, 28, 26)" fgColor="white"/>
       <SocialIcon url="https://www.linkedin.com/in/rahul-yadav-2a4b3b199/" bgColor="rgb(18, 28, 26)" fgColor="white"/>
-      <SocialIcon url="www.x.com" bgColor="rgb(18, 28, 26)" fgColor="whiteg" />
+      <SocialIcon url="www.x.com" bgColor="rgb(18, 28, 26)" fgColor="white" />
       </div>
       </header>
+      <main className='main'>
+         Site is under construction...
+      </main>
     </div>
   );
 }

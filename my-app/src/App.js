9d4bbf6520aed842bc4c152ac0679a122c9e09bd
Light <95582906+ligh1yagami@users.noreply.github.com>
2024-10-06 10:50:21 +0530

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <a href="#" className="gradient">rahulyadav.click</a>
+      <a href="#" className="gradient">rahulyadav</a>
       <div className="socials">
       <SocialIcon url="https://instagram.com/__rxhxl._" bgColor="rgb(18, 28, 26)" fgColor="white"/>
       <SocialIcon url="https://www.linkedin.com/in/rahul-yadav-2a4b3b199/" bgColor="rgb(18, 28, 26)" fgColor="white"/>
